0c0d97225dc9977932d3460a09435b2d0f67456d
Xinbei Cai <2405510353@qq.com>
2025-05-16 19:07:27 +0800

    update

--- train/Food.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=1, python main.py --target_domain=Grocery_and_Gourmet_Food --source_domain=Home_and_Kitchen --device=cuda
--- train/Kitchen.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=2, python main.py --target_domain=Home_and_Kitchen --source_domain=Grocery_and_Gourmet_Food --device=cuda
